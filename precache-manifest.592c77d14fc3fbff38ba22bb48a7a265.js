c124c6ddfe52b14f1f8bb3c968a52325944dd075
Arif Shaik <shaikarif00207@gmal.com>
2020-04-15 10:12:32 +0530

    Updates

---
+++ precache-manifest.592c77d14fc3fbff38ba22bb48a7a265.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2cfa92503962a8a7fe310288b534786",
+    "url": "/Sample_REST/index.html"
+  },
+  {
+    "revision": "c2dbb7c0b827c76d7473",
+    "url": "/Sample_REST/static/css/main.558f216d.chunk.css"
+  },
+  {
+    "revision": "ca78f3fc59e87e0e5bec",
+    "url": "/Sample_REST/static/js/2.136ec9ee.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Sample_REST/static/js/2.136ec9ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c2dbb7c0b827c76d7473",
+    "url": "/Sample_REST/static/js/main.15f8e744.chunk.js"
+  },
+  {
+    "revision": "a637b59e783c6a414034",
+    "url": "/Sample_REST/static/js/runtime-main.8466b012.js"
+  }
+]);
